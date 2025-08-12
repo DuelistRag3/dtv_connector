632884b2aecb37095b9ed434a225be4e92030e6a
DuelistRag3 <christian.dullin@web.de>
2025-08-12 13:08:15 +0200

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 import socket
 from dotenv import load_dotenv
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/<address>/<port>')
 def connect(address, port):
     ip = socket.gethostbyname(address)
-    return f"Connecting to {ip}:{port}"
+    return redirect(f"steam://connect/{ip}:{port}")
 
 if __name__ == '__main__':
     app.run(debug=True, host=os.getenv("HOST"), port=os.getenv("PORT"))
